c1c032f315a0b1149ba70050c55d9cbef01818d1
sob2545 <sob2545@naver.com>
2022-07-13 23:36:41 +0900

    problem fix with param but have to fix mirror symmetry

--- srcs/liner.c
+++ srcs/liner.c
@@ -6,7 +6,7 @@
 /*   By: sesim <sesim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 09:06:24 by sesim             #+#    #+#             */
-/*   Updated: 2022/07/13 23:10:19 by sesim            ###   ########.fr       */
+/*   Updated: 2022/07/13 23:35:06 by sesim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	rotation(t_fdf *fdf)
 		k = -1;
 		while (++k < fdf->mlx->ucs.w)
 		{
-			if (fdf->mlx->handler.ro_mod == 0)
+			//if (fdf->mlx->handler.ro_mod == 0)
 				euler_rotate(fdf, j, k);
 			/*else if (fdf->mlx->handler.ro_mod == 1)
 				quaternion(fdf, &q, j, k);*/
@@ -53,19 +53,19 @@ t_vertex	set_vertex(t_fdf *fdf, int h, int w, int flag)
 	ft_bzero(&line, sizeof(t_vertex));
 	if (flag == 1)
 	{
-		line.x1 = fdf->point[h][w - 1].x * fdf->mlx->handler.scale;
-		line.y1 = fdf->point[h][w - 1].y * fdf->mlx->handler.scale;
-		line.x2 = fdf->point[h][w].y * fdf->mlx->handler.scale;
-		line.y2 = fdf->point[h][w].y * fdf->mlx->handler.scale;
+		line.x1 = fdf->point[h][w - 1].iso_x * fdf->mlx->handler.scale;
+		line.y1 = fdf->point[h][w - 1].iso_y * fdf->mlx->handler.scale;
+		line.x2 = fdf->point[h][w].iso_x * fdf->mlx->handler.scale;
+		line.y2 = fdf->point[h][w].iso_y * fdf->mlx->handler.scale;
 		line.rgb1.color = fdf->point[h][w - 1].color;
 		line.rgb2.color = fdf->point[h][w].color;
 	}
 	else
 	{
-		line.x1 = fdf->point[h - 1][w].x * fdf->mlx->handler.scale;
-		line.y1 = fdf->point[h - 1][w].y * fdf->mlx->handler.scale;
-		line.x2 = fdf->point[h][w].y * fdf->mlx->handler.scale;
-		line.y2 = fdf->point[h][w].y * fdf->mlx->handler.scale;
+		line.x1 = fdf->point[h - 1][w].iso_x * fdf->mlx->handler.scale;
+		line.y1 = fdf->point[h - 1][w].iso_y * fdf->mlx->handler.scale;
+		line.x2 = fdf->point[h][w].iso_x * fdf->mlx->handler.scale;
+		line.y2 = fdf->point[h][w].iso_y * fdf->mlx->handler.scale;
 		line.rgb1.color = fdf->point[h - 1][w].color;
 		line.rgb2.color = fdf->point[h][w].color;
 	}
